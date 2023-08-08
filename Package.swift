e9c1b8646a57c7c8d7dbf9d276259e7aea5d180c
Nghia Vo <nghiavo.live@gmail.com>
2023-08-08 15:10:03 +0700

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "FilesProvider",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "FilesProvider",
+            targets: ["FilesProvider"]
+        )
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "FilesProvider",
+                dependencies: [],
+                path: "Sources"
+        ),
+        .testTarget(name: "FilesProviderTests",
+                dependencies: ["FilesProvider"],
+                path: "Tests"
+        ),
+    ]
+)
